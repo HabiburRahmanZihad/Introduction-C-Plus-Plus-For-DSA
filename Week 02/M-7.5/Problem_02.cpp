9118470c9f5cba2ec5b864a92eb1fb01f76ea4f7
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-09-30 14:09:57 +0600

    Second problem solve done

--- Week 02/M-7.5/Problem_02.cpp
+++ Week 02/M-7.5/Problem_02.cpp
@@ -11,13 +11,14 @@ Note: name will not contain any spaces.
 
 
 Sample Input
-Sample Output
+
 5
 Asif 29 95
 Sakib 55 89
 Zubair 57 93
 Ahsan 39 86
 Joy 12 99
+Sample Output
 Joy 12 99
 Ahsan 39 86
 Zubair 57 93
@@ -28,3 +29,31 @@ Asif 29 95
 
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+        string name;
+        int roll;
+        int marks;
+};
+
+int main (){
+
+    int n;
+    cin>>n;
+    Student arr[n];
+    for(int i=0; i<n; i++){
+        string name;
+        int roll, marks;
+        cin>>name>>roll>>marks;
+        arr[i] = Student{name, roll, marks};
+    }
+
+    for(int i=n-1; i>=0; i--){
+        cout<<arr[i].name<<" "<<arr[i].roll<<" "<<arr[i].marks<<endl;
+    }
+
+    return 0;
+}
